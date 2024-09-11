903058c4783f64b7098e83f8dc3f5b225ef5cb09
seba0305 <sebastiandileva@gmail.com>
2024-09-10 21:23:42 -0300

    Database.sql

--- structure/Database.sql
+++ structure/Database.sql
@@ -2,112 +2,112 @@ CREATE DATABASE TRANSACCIONES_BANCO;
 
 USE TRANSACCIONES_BANCO;
 
-CREATE TABLE Transacciones (
-	Nro_Movimiento	INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE
-,   Fecha	DATE NOT NULL
-,	Importe	DECIMAL(12,2) NOT NULL
-,	Medio_Pago	INT	NOT NULL
-,	Codigo_Banco	INT	NOT NULL
-,	Codigo 	INT NOT NULL
+CREATE TABLE transacciones (
+	nro_movimiento	INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE
+,   	fecha	DATE NOT NULL
+,	importe	DECIMAL(12,2) NOT NULL
+,	medio_pago	INT	NOT NULL
+,	codigo_banco	INT	NOT NULL
+,	codigo 	INT NOT NULL
 );
 
-CREATE TABLE Proveedores (
-	Nro_Proveedor	INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-,	Raz_soc_Prov	VARCHAR(50)	NOT NULL
-,	Correo	VARCHAR(50)
+CREATE TABLE proveedores (
+	nro_proveedor	INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+,	raz_soc_prov	VARCHAR(50)	NOT NULL
+,	correo	VARCHAR(50)
 );
 
-CREATE TABLE Clientes (
-	Nro_Cliente	INT NOT NULL PRIMARY KEY	AUTO_INCREMENT
-,	Raz_soc_Cliente	VARCHAR(50)	NOT NULL
-,	Correo	VARCHAR(50)
+CREATE TABLE clientes (
+	nro_cliente	INT NOT NULL PRIMARY KEY	AUTO_INCREMENT
+,	raz_soc_cliente	VARCHAR(50)	NOT NULL
+,	correo	VARCHAR(50)
 );
 
-CREATE TABLE Empleados (
-	Legajo	INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-,	Nombre_Apellido	VARCHAR(50) NOT NULL
-,	Antiguedad INT
-,	Sector VARCHAR(50)
-,	Correo 	VARCHAR(50)
+CREATE TABLE empleados (
+	legajo	INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+,	nombre_apellido	VARCHAR(50) NOT NULL
+,	antiguedad INT
+,	sector VARCHAR(50)
+,	correo 	VARCHAR(50)
 );
 
-CREATE TABLE Bancos (
-	Cod_Banco	INT NOT NULL	PRIMARY KEY
-,	Nombre_Banco	VARCHAR(50)	NOT NULL
-,	Oficial	VARCHAR (50)
+CREATE TABLE bancos (
+	cod_banco	INT NOT NULL	PRIMARY KEY
+,	nombre_banco	VARCHAR(50)	NOT NULL
+,	oficial	VARCHAR (50)
 );
 
-CREATE TABLE Medios_de_Pago (
-	Codigo_Pago	INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-,	MEdio_Pago	VARCHAR(50)	NOT NULL
+CREATE TABLE medios_de_pago (
+	codigo_pago	INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+,	medio_pago	VARCHAR(50)	NOT NULL
 );
 
-CREATE TABLE Facturas(
-	Factura	INT NOT NULL PRIMARY KEY
-,	Codigo	INT NOT NULL
-,	Importe_factura	DECIMAL(12,2) NOT NULL
-,	Producto	VARCHAR(50) NOT NULL
-,	Cantidad	INT NOT NULL
+CREATE TABLE facturas(
+	factura	INT NOT NULL PRIMARY KEY
+,	codigo	INT NOT NULL
+,	importe_factura	DECIMAL(12,2) NOT NULL
+,	producto	VARCHAR(50) NOT NULL
+,	cantidad	INT NOT NULL
 );
 
-CREATE TABLE Enlace_Transacciones_Proveedores_Facturas (
-	Nro_Movimiento INT NOT NULL
-,	Nro_Proveedor INT NOT NULL
-,	Factura	INT NOT NULL
-,  PRIMARY KEY (Nro_Movimiento, Nro_Proveedor,Factura)
+CREATE TABLE enlace_transacciones_proveedores_facturas (
+	nro_movimiento INT NOT NULL
+,	nro_proveedor INT NOT NULL
+,	factura	INT NOT NULL
+,  PRIMARY KEY (nro_movimiento, nro_proveedor,factura)
 );
 
-CREATE TABLE Enlace_Transacciones_Clientes_Facturas (
-	Nro_Movimiento INT NOT NULL
-,	Nro_Cliente INT NOT NULL
-,	Factura	INT NOT NULL
-,  PRIMARY KEY (Nro_Movimiento, Nro_Cliente,Factura)
+CREATE TABLE enlace_transacciones_clientes_facturas (
+	nro_movimiento INT NOT NULL
+,	nro_cliente INT NOT NULL
+,	factura	INT NOT NULL
+,  PRIMARY KEY (nro_movimiento, nro_cliente,factura)
 );
 
-CREATE TABLE Enlace_Transacciones_Empleados (
-	Nro_Movimiento INT NOT NULL
-,	Legajo INT NOT NULL
-,  PRIMARY KEY (Nro_Movimiento, Legajo)
+CREATE TABLE enlace_transacciones_empleados (
+	nro_movimiento INT NOT NULL
+,	legajo INT NOT NULL
+,  PRIMARY KEY (nro_movimiento, legajo)
 );
 
-ALTER TABLE Enlace_Transacciones_Proveedores_Facturas
-ADD CONSTRAINT fk_Enlace_Transacciones_Proveedores_Facturas_Transacciones
-FOREIGN KEY (Nro_Movimiento) REFERENCES Transacciones(Nro_Movimiento);
+ALTER TABLE enlace_transacciones_proveedores_facturas
+ADD CONSTRAINT fk_enlace_transacciones_proveedores_facturas_transacciones
+FOREIGN KEY (nro_movimiento) REFERENCES transacciones(nro_movimiento);
 
-ALTER TABLE Enlace_Transacciones_Proveedores_Facturas
-ADD CONSTRAINT fk_Enlace_Transacciones_Proveedores_Facturas_Proveedores
-FOREIGN KEY (Nro_Proveedor) REFERENCES Proveedores(Nro_Proveedor);
+ALTER TABLE enlace_transacciones_proveedores_facturas
+ADD CONSTRAINT fk_enlace_transacciones_proveedores_facturas_proveedores
+FOREIGN KEY (nro_proveedor) REFERENCES proveedores(nro_proveedor);
 
-ALTER TABLE Enlace_Transacciones_Proveedores_Facturas
-ADD CONSTRAINT fk_Enlace_Transacciones_Proveedores_Facturas_Facturas
-FOREIGN KEY (Factura) REFERENCES Facturas(factura);
+ALTER TABLE enlace_transacciones_proveedores_facturas
+ADD CONSTRAINT fk_enlace_transacciones_proveedores_facturas_facturas
+FOREIGN KEY (factura) REFERENCES facturas(factura);
 
-ALTER TABLE Enlace_Transacciones_Clientes_Facturas
-ADD CONSTRAINT fk_Enlace_Transacciones_Clientes_Facturas_Transacciones
-FOREIGN KEY (Nro_Movimiento) REFERENCES Transacciones(Nro_Movimiento);
+ALTER TABLE enlace_transacciones_clientes_facturas
+ADD CONSTRAINT fk_enlace_transacciones_clientes_facturas_transacciones
+FOREIGN KEY (nro_movimiento) REFERENCES transacciones(nro_movimiento);
 
-ALTER TABLE Enlace_Transacciones_Clientes_Facturas
-ADD CONSTRAINT fk_Enlace_Transacciones_Clientes_Facturas_Clientes
-FOREIGN KEY (Nro_Cliente) REFERENCES Clientes(Nro_Cliente);
+ALTER TABLE enlace_transacciones_clientes_facturas
+ADD CONSTRAINT fk_enlace_transacciones_clientes_facturas_clientes
+FOREIGN KEY (nro_cliente) REFERENCES clientes(nro_cliente);
 
-ALTER TABLE Enlace_Transacciones_Clientes_Facturas
-ADD CONSTRAINT fk_Enlace_Transacciones_Clientes_Facturas_Facturas
-FOREIGN KEY (Factura) REFERENCES Facturas(Factura);
+ALTER TABLE enlace_transacciones_clientes_facturas
+ADD CONSTRAINT fk_enlace_transacciones_clientes_facturas_facturas
+FOREIGN KEY (factura) REFERENCES facturas(factura);
 
-ALTER TABLE Enlace_Transacciones_Empleados
-ADD CONSTRAINT fk_Enlace_Transacciones_Empleados_Transacciones
-FOREIGN KEY (Nro_Movimiento) REFERENCES Transacciones(Nro_Movimiento);
+ALTER TABLE enlace_transacciones_empleados
+ADD CONSTRAINT fk_enlace_transacciones_empleados_transacciones
+FOREIGN KEY (nro_movimiento) REFERENCES transacciones(nro_movimiento);
 
-ALTER TABLE Enlace_Transacciones_Empleados
-ADD CONSTRAINT fk_Enlace_Transacciones_Empleados_Empleados
-FOREIGN KEY (Legajo) REFERENCES Empleados(Legajo);
+ALTER TABLE enlace_transacciones_empleados
+ADD CONSTRAINT fk_enlace_transacciones_empleados_empleados
+FOREIGN KEY (legajo) REFERENCES empleados(legajo);
 
 ALTER TABLE transacciones
-ADD CONSTRAINT FK_TRANSACCIONES_MEDIODEPAGO
-FOREIGN KEY (Medio_Pago) REFERENCES medios_de_pago(Codigo_Pago)
+ADD CONSTRAINT fk_transacciones_mediodepago
+FOREIGN KEY (medio_pago) REFERENCES medios_de_pago(codigo_pago)
 ;
 
 ALTER TABLE transacciones
-ADD CONSTRAINT FK_TRANSACCIONES_BANCOS
-FOREIGN KEY (Codigo_Banco) REFERENCES bancos(Cod_Banco)
+ADD CONSTRAINT fk_transacciones_bancos
+FOREIGN KEY (codigo_banco) REFERENCES bancos(cod_banco)
 ;
